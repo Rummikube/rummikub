03dfd0a491e1c645a6a808902f77d4fb9ac73865
junhaa <2171326@hansung.ac.kr>
2023-12-10 22:20:16 +0900

    :sparkles: FEAT. 루미큐브 메인 로직 구현
    보드판, 유저, 타일 객체 클래스 구현
    
    Related to : #1

---
+++ src/Tile.java
@@ -0,0 +1,43 @@
+public class Tile {
+    private int number; // 타일의 숫자 만약, 부기(조커)타일이라면 -1
+    private String color; // 타일의 색상
+
+    // 생성자
+    public Tile(int number, String color) {
+        this.number = number;
+        this.color = color;
+    }
+
+    public Tile(int number) {
+        this.number = number;
+        color = "Bugi";
+    }
+
+
+    // 숫자를 반환하는 메서드
+    public int getNumber() {
+        return number;
+    }
+
+    // 색상을 반환하는 메서드
+    public String getColor() {
+        return color;
+    }
+
+    // 부기 타일 여부를 반환하는 메서드
+    public boolean isBugi() {
+        return (number == 1 && color.equals("Bugi"));
+    }
+
+    // 타일 정보를 문자열로 반환하는 메서드
+    @Override
+    public String toString() {
+        if(isBugi()) return "Tile{ BUGI }";
+        else {
+            return "Tile{" +
+                    "number=" + number +
+                    ", color='" + color + '\'' +
+                    "}";
+        }
+    }
+}
